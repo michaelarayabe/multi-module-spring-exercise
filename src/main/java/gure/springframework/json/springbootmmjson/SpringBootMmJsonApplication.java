0e54da06440602fcdd9034679fe16795bc7a45e1
michaelarayabe <michaelseifuaraya@gmail.com>
2021-01-08 09:12:11 +0100

    exercise first commit

---
+++ src/main/java/gure/springframework/json/springbootmmjson/SpringBootMmJsonApplication.java
@@ -0,0 +1,13 @@
+package gure.springframework.json.springbootmmjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMmJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMmJsonApplication.class, args);
+    }
+
+}
